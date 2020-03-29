1beb54573e8fea6765eff352b3bd606bb97ee7f4
kai815 <h.kaizuka815@gmail.com>
2020-03-29 19:36:28 +0900

    add component

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -3,7 +3,10 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header />
+    {/* propsで値が渡される */}
+    <Header headerText="About Gatsby" />
+    {/* 再利用できる */}
+    <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
   </div>
 )
