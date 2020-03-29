887e60cf8d08a62ec0d7b2e1bf8723483bc5d1b7
kai815 <h.kaizuka815@gmail.com>
2020-03-30 07:02:46 +0900

    style a component using CSS Modules

--- tutorial-part-two/src/pages/about-css-modules.js
+++ tutorial-part-two/src/pages/about-css-modules.js
@@ -26,6 +26,6 @@ export default () => (
             username="Bob Smith"
             avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
             excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
+        />
     </Container>
 )
